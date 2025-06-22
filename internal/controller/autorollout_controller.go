232e4b7a0d1f4699b71816d41245a1f55701d23a
Aditya Bhalerao <adityab7968@gmail.com>
2025-06-22 19:29:16 +0530

    Remove unused Kubebuilder scaffolding

--- internal/controller/autorollout_controller.go
+++ internal/controller/autorollout_controller.go
@@ -1,19 +1,3 @@
-/*
-Copyright 2025.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package controller
 
 import (
@@ -26,29 +10,26 @@ import (
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 )
 
-// AutoRolloutReconciler reconciles a AutoRollout object
+// AutoRolloutReconciler watches ConfigMaps and triggers rollouts.
 type AutoRolloutReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
 }
 
-// +kubebuilder:rbac:groups=core,resources=autorollouts,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=core,resources=autorollouts/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=core,resources=autorollouts/finalizers,verbs=update
+// +kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch
+// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;patch
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the AutoRollout object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.21.0/pkg/reconcile
 func (r *AutoRolloutReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = logf.FromContext(ctx)
+	log := logf.FromContext(ctx)
 
-	// TODO(user): your logic here
+	var cm corev1.ConfigMap
+	if err := r.Get(ctx, req.NamespacedName, &cm); err != nil {
+		// ConfigMap might have been deleted
+		log.Error(err, "unable to fetch ConfigMap")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	// TODO: find deployments using this ConfigMap and trigger rollout
 
 	return ctrl.Result{}, nil
 }
@@ -56,7 +37,7 @@ func (r *AutoRolloutReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 // SetupWithManager sets up the controller with the Manager.
 func (r *AutoRolloutReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&corev1.AutoRollout{}).
+		For(&corev1.ConfigMap{}).
 		Named("autorollout").
 		Complete(r)
 }
